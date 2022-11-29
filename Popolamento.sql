42801d99a32079a8419407746bf8157943259812
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-11-29 12:41:23 +0100

    new

--- Popolamento.sql
+++ Popolamento.sql
@@ -1,5 +1,4 @@
 --Fornitore
-
 insert into Fornitore (pIVA,nome,citta,cap,via) 
 values (01223580554,'AURIAN SAS','Condom',32100,'5 Avenue de la Gare');
 
@@ -9,10 +8,8 @@ values (01223580604,'ASIA EUROPE TRADE','Liege',4000,'25 rue de Rotterdam');
 insert into Fornitore (pIVA,nome,citta,cap,via)
 values (01223530003,'E-SARDINIA ','Oristano',09170,'Via Santa Petronilla');
 
-
 --Mastro Birraio
 --ssn è Americano (16 Alfanumerico) mentre quello Italiano (9 Numeri)
-
 insert into MastroBirraio (ssn,nome,cognome,dataNascita,dataAssunzione,stipendio)
 values ('134009342','Antonio','Mannoia',TO_DATE('10/04/1980','DD/MM/YYYY'),TO_DATE('21/05/2003','DD/MM/YYYY'),1600.00);
 
@@ -22,13 +19,11 @@ values ('238009300','Eva','Fognini',TO_DATE('15/01/1992','DD/MM/YYYY'),TO_DATE('
 insert into MastroBirraio (ssn,nome,cognome,dataNascita,dataAssunzione,stipendio)
 values ('364386542','Luigi','Folliero',TO_DATE('02/11/1989','DD/MM/YYYY'),TO_DATE('13/12/2009','DD/MM/YYYY'),1700.00);
 
-
 --Contenitore
 --id Generato di DEFAULT
 --ssnResponsabile l'ho copiato uguale ai precedenti Mastri birrai poichè fa riferimento a quello
 --Capacita espressa in Litri(L)
 --Potenza espressa in Watt(W)
-
 
 insert into Contenitore (ssnResponsabile,tipo,capacitaLavorazione,capacitaComplessiva,potenza)
 values ('134009342','Bollitore',80,100,3000);
---
+++ Trigger.sql
@@ -0,0 +1,14 @@
+/*  1) Il primo trigger controlla che la quantità di malto utilizzato per l'ammostamento sia disponibile
+       tra le scorte in magazzino poichè ovviamente non possiamo utilizzare una quantità maggiore di
+       quella che abbiamo in stock*/
+
+
+
+/*  2) Il secondo trigger controlla che la quantità di luppolo utilizzato per l'ammostamento sia 
+       disponibile tra le scorte in magazzino poichè ovviamente non possiamo utilizzare una quantità 
+       maggiore di quella che abbiamo in stock*/
+
+
+/*  3) Il terzo trigger controlla che la quantità di lievito utilizzato per la fermentazione sia 
+       disponibile tra le scorte in magazzino poichè ovviamente non possiamo utilizzare una quantità 
+       maggiore di quella che abbiamo in stock*/
