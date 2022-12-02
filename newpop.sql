24ff983bad442569ebb36e742438b60800465911
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-12-02 15:10:36 +0100

    new

---
+++ newpop.sql
@@ -0,0 +1,50 @@
+/*FORNITORI
+MASTRI BIRRAI
+CONTENITORI
+PUB*/
+
+--FORNITORE
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000011,'AURIAN SAS','Condom','32100','5 Avenue de la Gare');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000012,'ASIA EUROPE TRADE','Liege','4000','25 rue de Rotterdam');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000013,'E-SARDINIA ','Oristano','09170','Via Santa Petronilla 15');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000014,'NOSTRALE','Milano','20157','Via G. Ungaretti 23');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000015,'PRATOROSSO','Milano','20107','Cascina Gaita 5');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000016,'MARCONI','Novara','30040','5 Avenue de la Gare');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000017,'BRASSERIE DE BRUNEHAUT SA','Brunehaut','7623','17 rue des Panneries');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000018,'SWISS CHIPS GMBH','Schneisingen','5425','4 Schulstrasse');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000019,'RESONE','Paris','75008','1 Rue De Stockholm');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000020,'VETRARIA','Savignano Sul Rubicone','47039','Via Emilia Est 23');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000021,'BROUWERIJ DE FENIKS CVBA','Heule','8501','138 Mellestraat');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000022,'FLECKS BRAUHAUS TECHNIK GMBH ','Frohnleiten','8130','Rothleiten 64');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000023,'KURSK BREWERY, LLC','Kursk','30502','Magistralny 18');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000024,'BEERINBA DI BARBORA','Vigevano','27029','Via Carrobbio 18');
+
+insert into Fornitore(pIVA,nomeFornitore,citta,cap,via)
+values (00000000025,'DE-GUSTAMI','Milano','20131','Via Vallazze 95');
