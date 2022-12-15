015c2dc78d9cdda305765e52dba66b1fe9f8433c
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-12-15 17:03:51 +0100

    commit

--- Procedure.sql
+++ Procedure.sql
@@ -1,6 +1,3 @@
-/*1) Procedura che incomincia automaticamente la fermentazione 
-     dopo che avviene la fine dell'ammostamento7 */
-
 
 /*2) Trovare la birra che viene venduta di meno e applicare uno
      sconto del 25% sul prossimo acquisto del fusto di tale birra.*/
@@ -8,12 +5,8 @@
 
 /*3) Qualora le quantità in magazzino (Malto, Luppolo e Lievito) fossero pari a 0 (o inferiori a
      una soglia prestabilita) automaticamente tramite il codFattura effettureà un nuovo ordine 
-     di approvvigionamento delle materie prime. */
+     di approvvigionamento delle materie prime. ??????? */ 
 
 
 /*4) Il mastro birraio responsabile del tipo di birra più venduta riceverà un incremento dello 
      stipendio del 15%. */
-
-
-/*5) Il Pub che acquista un numero superiore di x fusti riceverà uno sconto del 10% sul prezzo 
-     finale.*/
